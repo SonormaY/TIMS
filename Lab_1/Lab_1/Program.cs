af806cfc359ce8c4388d3c6c8083659b2dc0dafb
Stanislav <144832051+SonormaY@users.noreply.github.com>
2024-02-17 21:13:19 +0200

    ver1

--- Lab_1/Lab_1/Program.cs
+++ Lab_1/Lab_1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Plotly.NET;
+using Plotly.NET.LayoutObjects;
 using Plotly.NET.TraceObjects;
+using LinearAxisTuple = System.Tuple<Plotly.NET.StyleParam.LinearAxisId, Plotly.NET.StyleParam.LinearAxisId>;
 using System;
-
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
 /*Згенерувати вибірку заданого об’єму (не менше 50) з вказаного проміжку для
 дискретної статистичної змінної. На підставі отриманих вибіркових даних:
  побудувати варіаційний ряд та частотну таблицю; представити графічно
@@ -42,7 +45,6 @@ for (int i = 0; i < n; i++)
 }
 System.Console.WriteLine();
 // Частотна таблиця
-System.Console.WriteLine("Частотна таблиця: ");
 var dict = new System.Collections.Generic.Dictionary<int, int>();
 for (int i = 0; i < n; i++)
 {
@@ -61,23 +63,23 @@ for (int i = 0; i < dict.Keys.ToArray().Length; i++)
 {
     headers[i] = new int[]{dict.Keys.ToArray()[i]};
 }
-ChartDomain.Chart.Table<int[], int, int[], int>(
+var table = ChartDomain.Chart.Table<int[], int, int[], int>(
     headerValues: headers,
-    cellsValues: new int[] []{
+    cellsValues: new int[][]{
         dict.Values.ToArray()
     }
-).Show();
+);
 
 // Графік
-Chart2D.Chart.Line<int, int, int>(
+var poligon = Chart2D.Chart.Line<int, int, int>(
     x: dict.Keys,
     y: dict.Values.ToArray()
-).Show();
+);
 
-Chart2D.Chart.Column<int, int, int ,int, int>(
+var diagram = Chart2D.Chart.Column<int, int, int, int, int>(
     values: dict.Values,
     Keys: dict.Keys.ToArray()
-).Show();
+);
 
 // Емпірична функція розподілу
 var emp = new double[dict.Keys.Count];
@@ -86,12 +88,55 @@ for (int i = 0; i < dict.Keys.Count; i++)
     emp[i] = dict.Values.Take(i + 1).Sum() / (double)n;
 }
 var lines = new List<GenericChart.GenericChart>();
-for (int i = 0; i < dict.Keys.Count - 1; i++)
+for (int i = 0; i < dict.Keys.Count; i++)
 {
     lines.Add(Chart2D.Chart.Line<int, double, int>(
-        x: new int[2]{dict.Keys.ToArray()[i], dict.Keys.ToArray()[i + 1]},
+        x: new int[2]{dict.Keys.ToArray()[i] - 1, dict.Keys.ToArray()[i]},
         y: new double[2] { emp[i], emp[i] }
     ));
-}    
-Chart.Combine(lines).Show();
+}
+var empiric_func = Chart.Combine(lines);
+
+Chart.Grid<IEnumerable<GenericChart.GenericChart>>(2,2).Invoke(new[] { poligon, diagram, empiric_func, table }).Show();
+
+// Числові характеристики
+Console.WriteLine("Центральної тенденції");
+Console.WriteLine("Медіана:");
+if (n % 2 == 0)
+{
+    Console.WriteLine((sample[n / 2] + sample[n / 2 - 1]) / 2);
+}
+else
+{
+    Console.WriteLine(sample[n / 2]);
+}
+Console.WriteLine($"Середнє арифметичне: {sample.Average()}");
+var max = 0;
+var mode = 0;
+foreach (var item in dict)
+{
+    if (item.Value > max)
+    {
+        max = item.Value;
+        mode = item.Key;
+    }
+}
+Console.WriteLine($"Мода: {mode}");
+Console.WriteLine("Розсіювання");
+var d = 0.0;
+for (int i = 0; i < n; i++)
+{
+    d += Math.Pow(sample[i] - sample.Average(), 2);
+}
+Console.WriteLine($"Девіація: {d}");
+var vs = d / (n - 1);
+Console.WriteLine($"Варіанса: {vs}");
+var s = Math.Sqrt(vs);
+Console.WriteLine($"Стандарт: {s}");
+Console.WriteLine($"Розмах: {Math.Abs(end - start)}");
+Console.WriteLine($"Коефіцієнт варіації: {s / sample.Average()}");
+Console.WriteLine("Введіть а: ");
+var a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Квантиль порядку");
 
--- Lab_1/Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
+++ Lab_1/Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50026abe9f98ff05d50472cf39de13cda95bff55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
