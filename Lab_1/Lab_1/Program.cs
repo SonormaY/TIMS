fe16a6b1e7cbb4de5e9d804070b31fb491556270
¨Stanislav¨ <stanislav.humeniuk@outlook.com>
2024-02-20 16:40:38 +0200

    fin

--- Lab_1/Lab_1/Program.cs
+++ Lab_1/Lab_1/Program.cs
@@ -4,14 +4,20 @@ using Plotly.NET.TraceObjects;
 using LinearAxisTuple = System.Tuple<Plotly.NET.StyleParam.LinearAxisId, Plotly.NET.StyleParam.LinearAxisId>;
 using System;
 using System.Text;
-Console.OutputEncoding = Encoding.UTF8;
-/*Згенерувати вибірку заданого об’єму (не менше 50) з вказаного проміжку для
-дискретної статистичної змінної. На підставі отриманих вибіркових даних:
- побудувати варіаційний ряд та частотну таблицю; представити графічно
-статистичний матеріал, побудувати графік емпіричної функції розподілу
-обчислити числові характеристики дискретного розподілу.*/
+using System.Diagnostics.Metrics;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-static void Zavd1(){
+Console.OutputEncoding = Encoding.UTF8;
+
+static void Task1(){
+    Console.Clear();
+    /*Згенерувати вибірку заданого об’єму (не менше 50) з вказаного проміжку для
+    дискретної статистичної змінної. На підставі отриманих вибіркових даних:
+     побудувати варіаційний ряд та частотну таблицю; представити графічно
+    статистичний матеріал, побудувати графік емпіричної функції розподілу
+    обчислити числові характеристики дискретного розподілу.*/
+
     // Задання вибірки
     Console.WriteLine("Введіть початок проміжку: ");
     var start = Convert.ToInt32(Console.ReadLine());
@@ -39,12 +45,12 @@ static void Zavd1(){
 
     // Варіаційний ряд
     Array.Sort(sample);
-    System.Console.WriteLine("Варіаційний ряд: ");
+    Console.WriteLine("Варіаційний ряд: ");
     for (int i = 0; i < n; i++)
     {
-        System.Console.Write(sample[i] + " ");
+        Console.Write(sample[i] + " ");
     }
-    System.Console.WriteLine();
+    Console.WriteLine();
 
     // Частотна таблиця
     var dict = new System.Collections.Generic.Dictionary<int, int>();
@@ -106,11 +112,11 @@ static void Zavd1(){
     Console.WriteLine("Центральної тенденції");
     if (n % 2 == 0)
     {
-        Console.WriteLine($"Медіана{(sample[n / 2] + sample[n / 2 - 1]) / 2}");
+        Console.WriteLine($"Медіана {sample[(n / 2) - 1]}, {sample[n / 2]}");
     }
     else
     {
-        Console.WriteLine(sample[n / 2]);
+        Console.WriteLine(sample[(int)Math.Floor(n / 2.0)]);
     }
     var avg = sample.Average();
     Console.WriteLine($"Середнє арифметичне: {avg}");
@@ -138,12 +144,43 @@ static void Zavd1(){
     Console.WriteLine($"Стандарт: {s}");
     Console.WriteLine($"Розмах: {Math.Abs(end - start)}");
     Console.WriteLine($"Коефіцієнт варіації: {s / avg}");
-    Console.WriteLine("Введіть а: ");
-    var a = Convert.ToInt32(Console.ReadLine());
-    var kvant = sample[(int)((a * n)/100) - 1];
-    Console.WriteLine($"Квантиль порядку {a}: {kvant}");
-
-    System.Console.WriteLine("Коефіцієнти асиметрії та ексцесу");
+    Console.WriteLine("Вибіркова дисперсія: " + d / n);
+    Console.WriteLine("Вибіркове середнє квадратичне відхилення: " + Math.Sqrt(d / n));
+    
+    System.Console.WriteLine("Квантилі: ");
+    if (n % 4 == 0)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Q{i} квартиль: {sample[((n / 4) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n % 8 == 0)
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            System.Console.WriteLine($"Q{i} октиль: {sample[((n / 8) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n % 10 == 0)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            System.Console.WriteLine($"Q{i} дециль: {sample[((n / 10) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n % 100 == 0)
+    {
+        for (int i = 1; i <= 99; i++)
+        {
+            System.Console.WriteLine();
+        }
+    }
+    System.Console.WriteLine();
+    Console.WriteLine("Коефіцієнти асиметрії та ексцесу");
     var a1 = 0.0;
     var a2 = 0.0;
     var a3 = 0.0;
@@ -162,50 +199,218 @@ static void Zavd1(){
     Console.WriteLine($"Коефіцієнт ексцесу: {exc}");
 }
 
-/*2. Згенерувати вибірку заданого об’єму (не менше 50) з вказаного проміжку для 
-неперервної статистичної змінної. На підставі отриманих вибіркових даних: 
- утворити інтервальний статистичний розподіл, побудувати гістограму та 
-графік емпіричної функції розподілу, обчислити числові характеристики. */
+static void Task2(){
+    Console.Clear();
+    /*2. Згенерувати вибірку заданого об’єму (не менше 50) з вказаного проміжку для 
+    неперервної статистичної змінної. На підставі отриманих вибіркових даних: 
+     утворити інтервальний статистичний розподіл, побудувати гістограму та 
+    графік емпіричної функції розподілу, обчислити числові характеристики. */
 
-Console.WriteLine("Введіть початок проміжку: ");
-var start1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введіть початок проміжку: ");
+    var start1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введіть кінець проміжку: ");
-var end1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введіть кінець проміжку: ");
+    var end1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введіть об'єм вибірки: ");
-var n1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введіть об'єм вибірки: ");
+    var n1 = Convert.ToInt32(Console.ReadLine());
 
-var rand1 = new Random();
-var sample1 = new double[n1];
-for (int i = 0; i < n1; i++)
-{
-    sample1[i] = rand1.NextDouble() * (end1 - start1) + start1;
+    var rand1 = new Random();
+    var sample1 = new double[n1];
+    for (int i = 0; i < n1; i++)
+    {
+        sample1[i] = Math.Round(rand1.NextDouble() * (end1 - start1) + start1, 3);
+    }
+
+    // Вибірка
+    System.Console.WriteLine("Вибірка: ");
+    for (int i = 0; i < n1; i++)
+    {
+        System.Console.Write(sample1[i] + " ");
+    }
+    // Варіаційний ряд
+    Array.Sort(sample1);
+    System.Console.WriteLine();
+    // Інтервальний статистичний розподіл
+    var k = (int)Math.Ceiling(Math.Log(n1, 2));
+    var h = (sample1.Max() - sample1.Min()) / k;
+    var intervals = new double[k];
+    for (int i = 0; i < k; i++)
+    {
+        intervals[i] = Math.Round(sample1.Min() + h * (i + 1), 3);
+    }
+    var hist = new System.Collections.Generic.Dictionary<double, int>();
+    for (int i = 0; i < k; i++)
+    {
+        hist.Add(intervals[i], 0);
+    }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            if (sample1[i] <= intervals[j])
+            {
+                hist[intervals[j]]++;
+                break;
+            }
+        }
+    }
+    var headers = new string[hist.Keys.Count] [];
+    for (int i = 0; i < hist.Keys.ToArray().Length; i++)
+    {
+        headers[i] = new string[]{$"({Math.Round(hist.Keys.ToArray()[i] - h, 3)}; {Math.Round(hist.Keys.ToArray()[i], 3)}]"};
+    }
+    var table = ChartDomain.Chart.Table<string[], string, int[], int>(
+        headerValues: headers,
+        cellsValues: new int[][]{
+            hist.Values.ToArray()
+        }
+    );
+
+    var histogram = Chart2D.Chart.Histogram<double, int>(
+        data: sample1,
+        XBins: Bins.init(Start: sample1.Min(), End: sample1.Max(), Size: h + 0.001d),
+        orientation: StyleParam.Orientation.Vertical
+    );
+
+    var sample_mid = new System.Collections.Generic.Dictionary<double, int>();
+    for (int i = 0; i < k; i++)
+    {
+        sample_mid.Add(i * h - h / 2 ,hist[intervals[i]]);
+    }
+
+    // Емпірична функція розподілу
+    var emp = new double[sample_mid.Keys.Count];
+        for (int i = 0; i < sample_mid.Keys.Count; i++)
+        {
+            emp[i] = sample_mid.Values.Take(i + 1).Sum() / (double)n1;
+        }
+        var lines = new List<GenericChart.GenericChart>();
+        for (int i = 0; i < sample_mid.Keys.Count; i++)
+        {
+            lines.Add(Chart2D.Chart.Line<double, double, int>(
+                x: new double[2]{sample_mid.Keys.ToArray()[i] - h, sample_mid.Keys.ToArray()[i]},
+                y: new double[2] { emp[i], emp[i] }
+            ));
+        }
+        var empiric_func = Chart.Combine(lines);
+
+    Chart.Grid<IEnumerable<GenericChart.GenericChart>>(3, 1).Invoke(new[] { table, histogram, empiric_func}).Show();
+
+    // Числові характеристики
+    Console.WriteLine("Центральної тенденції");
+    var avg1 = sample1.Average();
+    Console.WriteLine($"Середнє арифметичне: {avg1}");
+    System.Console.WriteLine("Медіана: ");
+    if (n1 % 2 == 0)
+    {
+        Console.WriteLine($"Медіана {sample1[(n1 / 2) - 1]}, {sample1[n1 / 2]}");
+    }
+    else
+    {
+        Console.WriteLine($"Медіана: {sample1[(int)Math.Floor(n1 / 2.0)]}");
+    }
+    var max1 = 0;
+    var mode1 = 0.0;
+    foreach (var item in hist)
+    {
+        if (item.Value > max1)
+        {
+            max1 = item.Value;
+            mode1 = item.Key;
+        }
+    }
+    System.Console.WriteLine($"Мода: {mode1}");
+    Console.WriteLine("Розсіювання");
+    var d1 = 0.0;
+    for (int i = 0; i < n1; i++)
+    {
+        d1 += Math.Pow(sample1[i] - avg1, 2);
+    }
+    Console.WriteLine($"Девіація: {d1}");
+    var vs1 = d1 / (n1 - 1);
+    Console.WriteLine($"Варіанса: {vs1}");
+    var s1 = Math.Sqrt(vs1);
+    Console.WriteLine($"Стандарт: {s1}");
+    Console.WriteLine($"Розмах: {Math.Abs(sample1.Max() - sample1.Min())}");
+    Console.WriteLine($"Коефіцієнт варіації: {s1 / avg1}");
+    Console.WriteLine($"Вибіркова дисперсія: {d1 / n1}");
+    Console.WriteLine($"Вибіркове середнє квадратичне відхилення: {Math.Sqrt(d1 / n1)}");
+    System.Console.WriteLine("Квантилі: ");
+    if (n1 % 4 == 0)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Q{i} квартиль: {sample1[((n1 / 4) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n1 % 8 == 0)
+    {
+        for (int i = 1; i <= 7; i++)
+        {
+            System.Console.WriteLine($"Q{i} октиль: {sample1[((n1 / 8) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n1 % 10 == 0)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            System.Console.WriteLine($"Q{i} дециль: {sample1[((n1 / 10) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    if (n1 % 100 == 0)
+    {
+        for (int i = 1; i <= 99; i++)
+        {
+            System.Console.WriteLine($"Q{i} центиль: {sample1[((n1 / 100) * i) - 1]}");
+        }
+    }
+    System.Console.WriteLine();
+    Console.WriteLine("Коефіцієнти асиметрії та ексцесу");
+    var a11 = 0.0;
+    var a21 = 0.0;
+    var a31 = 0.0;
+    for (int i = 0; i < n1; i++)
+    {
+        a11 += Math.Pow(sample1[i] - avg1, 3);
+        a21 += Math.Pow(sample1[i] - avg1, 2);
+        a31 += Math.Pow(sample1[i] - avg1, 4);
+    }
+    a11 /= n1;
+    a21 /= n1;
+    a31 /= n1;
+    var asym1 = a11 / Math.Pow(a21, 1.5);
+    Console.WriteLine($"Коефіцієнт асиметрії: {asym1}");
+    var exc1 = a31 / Math.Pow(a21, 2) - 3;
+    Console.WriteLine($"Коефіцієнт ексцесу: {exc1}");
+
 }
 
-// Вибірка
-System.Console.WriteLine("Вибірка: ");
-for (int i = 0; i < n1; i++)
-{
-    System.Console.Write(sample1[i] + " ");
-}
-System.Console.WriteLine();
-// Варіаційний ряд
-Array.Sort(sample1);
-System.Console.WriteLine("Варіаційний ряд: ");
-for (int i = 0; i < n1; i++)
-{
-    System.Console.Write(sample1[i] + " ");
-}
-System.Console.WriteLine();
-// Інтервальний статистичний розподіл
-var k = (int)Math.Ceiling(Math.Log(n1, 2));
-var h = (sample1.Max() - sample1.Min()) / k;
-var freq = new int[k];
-// for (int i = 0; i < n1; i++)
-// {
-//     if(sample1[i]){
-        
-//     }
-// }
+while(true){
+    Console.Clear();
+    Console.WriteLine("Choose a task:");
+    Console.WriteLine("1. Task 1");
+    Console.WriteLine("2. Task 2");
 
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            Task1();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            break;
+        case 2:
+            Task2();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+}
--- Lab_1/Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
+++ Lab_1/Lab_1/obj/Debug/net8.0/Lab_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af806cfc359ce8c4388d3c6c8083659b2dc0dafb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6988c3811b82a85f3882fb35c4aa4fc3438a2519")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
